5e39ba43ee28f5181efa5037f54d886b0b2d2752
Mario <marioluengo3d@gmail.com>
2024-06-10 21:35:15 +0200

    Tracker Overview Screen

--- tracker/tracker_presentation/src/main/java/com/mluengo/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/mluengo/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,15 +1,26 @@
 package com.mluengo.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mluengo.core.util.UiEvent
 import com.mluengo.core_ui.LocalSpacing
+import com.mluengo.tracker_presentation.tracker_overview.components.AddButton
+import com.mluengo.tracker_presentation.tracker_overview.components.DaySelector
+import com.mluengo.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.mluengo.tracker_presentation.tracker_overview.components.NutrientsHeader
+import com.mluengo.tracker_presentation.tracker_overview.components.TrackedFoodItems
+import com.plcoding.core.R
 
 @Composable
 fun TrackerOverviewScreen(
@@ -26,6 +37,56 @@ fun TrackerOverviewScreen(
     ) {
         item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick) },
+                onNextDayClick = { viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal -> 
+            ExpandableMeal(
+                meal = meal,
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach { food ->
+                            TrackedFoodItems(
+                                trackedFood = food,
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent
+                                            .OnDeleteTrackedFoodClick(food)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent
+                                        .OnAddFoodClick(meal)
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
+                onToggleClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal)) },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
