d14606d1023e97aa40c796ce22b0efce3db6fc01
Mario <marioluengo3d@gmail.com>
2024-06-11 20:54:50 +0200

    Search viewmodel

---
+++ tracker/tracker_presentation/src/main/java/com/mluengo/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.mluengo.tracker_presentation.search
+
+import com.mluengo.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
